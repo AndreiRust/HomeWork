335886c3634b717750d7045176d8570e02b39381
AndreiRust <143093777+AndreiRust@users.noreply.github.com>
2023-08-28 16:04:57 +0300

    Add files via upload

---
+++ Lesson_1.java
@@ -0,0 +1,29 @@
+package AnDRuS.LessonKyrs;
+
+public class Lesson_1 {public static void main(String[] args) {
+    String printThreeWords = (" Orange \n Banana \n Apple ");//1
+    System.out.println(printThreeWords);
+
+
+    int checkSumSign;//2
+    int a = 5;
+    int b = 9;
+    checkSumSign = a + b;
+    System.out.println(checkSumSign);
+    System.out.println("Сумма положительная");
+
+
+    String printColor;//3
+    int value = 59;
+    printColor = "Желтый";
+    System.out.println(printColor);
+
+
+    String compareNumbers;//4
+    int A = 7;
+    int B = 8;
+    compareNumbers = "A < B";
+    System.out.println(compareNumbers);
+}
+}
+
